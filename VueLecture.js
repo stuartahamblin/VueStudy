75164b77b210f178a4e84c923204b66cb393d44e
stuartahamblin <stuart.a.hamblin@gmail.com>
2018-12-28 15:42:17 -0600

    exercise start

--- VueLecture.js
+++ VueLecture.js
@@ -1,35 +1,21 @@
-var inputEl = $('input');
-var buttonEl = $('button');
-var ulEl = $('ul');
-var todos = [];
-
-buttonEl.click(addTodo);
-
-function addTodo(){
-  var userInput = inputEl.val();
-  if(userInput.trim() == ''){
-    return;
-  }
-  var newTodo = { id: _.uniqueId(), value: userInput
-  };
-  todos.push(newTodo);
-  $('<li>'+ userInput + '</li>')
-    .appendTo(ulEl)
-    .attr('dataset-id', newTodo.id)
-    .click(removeTodo);
-  console.log(todos);
-}
-
-function removeTodo(event){
-  var clickedLi = $(this);
-  var itemId = clickedLi.attr('dataset-id');
-  console.log(clickedLi);
-  for (var i = 0; i < todos.length; i++){
-    if (todos[i].id == itemId) {
-      todos.splice(i, 1);
-      break;
+new Vue({
+  el: '#app',
+  data: {
+    todos: [],
+    userInput: ''
+  },
+  methods: {
+    addTodo: function(){
+      if(this.userInput.trim() == ''){
+        return;
+      }
+      var newTodo = {
+        id: _.uniqueId(),
+        value: this.userInput
+      };
+      this.todos.push(newTodo);
+      console.log(this.todos);
     }
   }
-  clickedLi.remove();
-  console.log(todos);
-}
+});
+
